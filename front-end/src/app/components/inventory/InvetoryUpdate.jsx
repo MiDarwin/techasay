a56243c1feaf14f5777231c32ef201c133b8df7c
MiDarwin <emirhanhorasan20@gmail.com>
2025-05-07 13:00:28 +0300

    Update InvetoryUpdate.jsx

--- front-end/src/app/components/inventory/InvetoryUpdate.jsx
+++ front-end/src/app/components/inventory/InvetoryUpdate.jsx
@@ -19,10 +19,12 @@ const modalStyle = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 500,
+  maxHeight: "75vh", // sayfanın yükseklik oranına göre sınır
   bgcolor: "background.paper",
   borderRadius: 2,
   boxShadow: 24,
   p: 4,
+  overflowY: "auto", // içerik taşarsa scroll
 };
 
 export default function InventoryUpdateModal({
@@ -77,54 +79,87 @@ export default function InventoryUpdateModal({
 
   return (
     <Modal open={open} onClose={onClose} disablePortal>
-      <Box sx={modalStyle}>
-        <Typography color="black" variant="h6" mb={2}>
-          Envanter #{inventory?.id} Düzenle
-        </Typography>
-
-        <Grid container spacing={2}>
-          {fields.map((fld, idx) => (
-            <React.Fragment key={idx}>
-              <Grid item xs={5}>
-                <TextField
-                  label="Anahtar"
-                  value={fld.key}
-                  onChange={(e) =>
-                    handleFieldChange(idx, "key", e.target.value)
-                  }
-                  fullWidth
-                />
+      <Box
+        sx={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 500,
+          maxHeight: "80vh",
+          bgcolor: "background.paper",
+          borderRadius: 2,
+          boxShadow: 24,
+          overflow: "hidden",
+        }}
+      >
+        {/* Scrollable iç bölüm: sadece orta kısmı kaydırılır */}
+        <Box
+          sx={{
+            maxHeight: "calc(80vh - 64px)",
+            overflowY: "auto",
+            borderTopLeftRadius: 2,
+            borderTopRightRadius: 2,
+          }}
+        >
+          <Box sx={{ p: 2 }}>
+            <Typography color="black" variant="h6" mb={2}>
+              Envanter #{inventory?.id} Düzenle
+            </Typography>
+            <Grid container spacing={2}>
+              {fields.map((fld, idx) => (
+                <React.Fragment key={idx}>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Anahtar"
+                      value={fld.key}
+                      onChange={(e) =>
+                        handleFieldChange(idx, "key", e.target.value)
+                      }
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={5}>
+                    <TextField
+                      label="Değer"
+                      value={fld.value}
+                      onChange={(e) =>
+                        handleFieldChange(idx, "value", e.target.value)
+                      }
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid item xs={2} display="flex" alignItems="center">
+                    <IconButton onClick={() => removeField(idx)} color="error">
+                      <RemoveCircleIcon />
+                    </IconButton>
+                  </Grid>
+                </React.Fragment>
+              ))}
+              <Grid item xs={12}>
+                <Button
+                  startIcon={<AddCircleIcon />}
+                  onClick={addField}
+                  variant="text"
+                >
+                  Alan Ekle
+                </Button>
               </Grid>
-              <Grid item xs={5}>
-                <TextField
-                  label="Değer"
-                  value={fld.value}
-                  onChange={(e) =>
-                    handleFieldChange(idx, "value", e.target.value)
-                  }
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={2} display="flex" alignItems="center">
-                <IconButton onClick={() => removeField(idx)} color="error">
-                  <RemoveCircleIcon />
-                </IconButton>
-              </Grid>
-            </React.Fragment>
-          ))}
-
-          <Grid item xs={12}>
-            <Button
-              startIcon={<AddCircleIcon />}
-              onClick={addField}
-              variant="text"
-            >
-              Alan Ekle
-            </Button>
-          </Grid>
-        </Grid>
-
-        <Box mt={3} display="flex" justifyContent="flex-end" gap={1}>
+            </Grid>
+          </Box>
+        </Box>
+        {/* Sabit footer */}
+        <Box
+          sx={{
+            p: 2,
+            borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+            display: "flex",
+            justifyContent: "flex-end",
+            gap: 1,
+            borderBottomLeftRadius: 2,
+            borderBottomRightRadius: 2,
+          }}
+        >
           <Button onClick={onClose}>İptal</Button>
           <Button variant="contained" onClick={handleSubmit}>
             Güncelle
